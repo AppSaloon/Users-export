f26b8606f8d2085ffe01fbf5fbc0a4f4858b997d
Aytac Kokus <aytac@appsaloon.be>
2020-02-20 15:44:54 +0100

    initial push

---
+++ as-user-export.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: User Export - AppSaloon
+Plugin URI:
+Description: Exports user to csv file
+Version: 1.0.0
+Author: AppSaloon
+Author URI: https://www.appsaloon.be/
+*/
+
+define( 'ASUX_PLUGIN_FILE', __FILE__ );
+define( 'ASUX_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'ASUX_PLUGIN_SLUG', dirname( plugin_basename( __FILE__ ) ) );
+define( 'ASUX_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'ASUX_PLUGIN_VERSION', '1.0.0' );
+
+require ASUX_PLUGIN_DIR . 'vendor/autoload.php';
